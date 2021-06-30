b619b941853a5e06903bf4601ca197a4c7d33e29
SwammerzzOP <76239041+Swam127@users.noreply.github.com>
2021-06-30 11:41:15 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,63 @@
+objectDetector = "";
+
+img = "";
+status = "";
+objects = [];
+
+function preload()
+{
+
+}
+
+function setup()
+{
+    canvas = createCanvas(380, 380);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(380, 380);
+    video.hide()
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function draw()
+{
+    image(video, 0, 0, 380, 380);
+
+    if (status != "")
+    {
+        r = random(255);
+        g = random(255);
+        b = random(255);
+        objectDetector.detect(video, gotResult)
+        for (s = 0; s < objects.length; s++)
+        {
+            document.getElementById("status").innerHTML = "Status : Object Detected";
+            document.getElementById("number_of_objects").innerHTML = "Number of objects detected are : "+objects.length;
+
+            fill(r, g, b);
+            percentage = floor(objects[s].confidence * 100);
+            text(objects[s].label + " " + percentage + "%", objects[s].x + 15, objects[s].y + 15);
+            noFill();
+            stroke(r, g, b);
+            rect(objects[s].x, objects[s].y, objects[s].width, objects[s].height);
+        }
+    }
+
+}
+
+function modelLoaded()
+{
+    console.log("Model Loaded !");
+    status = true;
+    objectDetector.detect(video, gotResult);
+}
+
+function gotResult(error, results)
+{
+    if (error) {
+          console.error(error)
+    } 
+        console.log(results);
+        objects = results;
+}
